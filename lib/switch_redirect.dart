31b9c7b5304f7c4afb9fa7267727af1861177d58
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-05-16 21:16:41 +0900

    Fix: 軽微なUI

--- lib/switch_redirect.dart
+++ lib/switch_redirect.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/main.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:mobile/utils/github.dart';
+import 'package:mobile/utils/users.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SwitchPage extends StatefulWidget {
   final Uri uri;
@@ -23,31 +27,67 @@ class _SwitchPageState extends State<SwitchPage> {
   }
 
   void handleRedirect() async {
+    final session = Supabase.instance.client.auth.currentSession;
     final queryParameters = widget.uri.queryParameters;
     final state = queryParameters['state'] ?? '';
     final stateComponents = state.split(',');
     
     // ディープリンクURIの取得（state パラメータの2番目の要素）
     final String? openMode = stateComponents.length > 1 ? stateComponents[1] : null;
-    if (kIsWeb && openMode == "mobile") {
-      // モバイルアプリの場合は直接ディープリンクを開く
-        setState(() {
-          message = Text("ここまではきてる", style:TextStyle(color: Colors.white));
-        });
-      final Uri uri = Uri(scheme: "enpower",path:widget.uri.path,queryParameters: widget.uri.queryParameters);
-      try{
-        setState((){
-          message = ElevatedButton(
-            onPressed: () async {
-              await launchUrl(uri);
-            },
-            child: Text('アプリを開く'),
-          );
-        });
-      }catch(e){
-        setState(() {
-          message = Text("リダイレクトに失敗しました:$uri,$e", style:TextStyle(color: Colors.white));
-        });
+    if (kIsWeb) {
+      if(openMode == null){
+        // モバイルアプリの場合は直接ディープリンクを開く
+        final Uri uri = Uri(scheme: "enpower",path:widget.uri.path,queryParameters: widget.uri.queryParameters);
+        try{
+          setState((){
+            message = ElevatedButton(
+              onPressed: () async {
+                await launchUrl(uri);
+              },
+              child: Text('アプリを開く'),
+            );
+          });
+        }catch(e){
+          setState(() {
+            message = Text("リダイレクトに失敗しました:$uri,$e", style:TextStyle(color: Colors.white));
+          });
+        }
+      }else{
+        if (widget.uri.scheme == "enpower" && widget.uri.path == "/redirect") {
+          final queryParameters = widget.uri.queryParameters;
+          final res = await getGitHubAccessToken(code: queryParameters['code'] ?? '', redirectUri: "https://mokuhub.vercel.app/redirect");
+          print(res);
+          try{
+            if(res != null){
+              final user = await getGitHubUser(res);
+              print(user?.id);
+              updateUser(
+                userId:session?.user.userMetadata?["provider_id"],
+                githubId: user?.id.toString(),
+              );
+              setState((){
+                message=Column(
+                  children:[
+                    Text("GitHubアカウント「${user?.name}」と連携が完了しました。"),
+                    ElevatedButton(
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      }, 
+                      child: Text("ホームに戻る")
+                    )
+                  ]
+                );
+              });
+            }
+          }catch(e){
+            print(e);
+          }
+        }else{
+          print("対応してないよーん");
+        }
       }
     }
   }
